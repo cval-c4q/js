b5127b8951454fbc22e277c2a4f6957a3f9b37b4
Carlo Valenti <carlovalenti@ac.c4q.nyc>
2017-10-03 14:27:38 -0400

    hash2T: document interface in JsDoc format

--- hashtable/hash2T.js
+++ hashtable/hash2T.js
@@ -1,19 +1,28 @@
 /**
- *   hash2T is implemented with index synced array with keys sorted in increasing order.
+ *   hash2T Hash table with two synced, sorted arrays
+ *   @module hash2T
+ *
  *   Usage: var Hash2T = require('./hash2T.js');
  *          var h = new Hash2T();
- *   Interface:
- *          Hash2T.set(key,val)		// set hash[key] to val
- *          Hash2T.unset(key)		// delete hash[key] if any
- *          Hash2T.get(key)		// lookup hash[key]
- *          Hash2T.find(val)		// reverse lookup, returns first matching key
  */
 
-function Hash2T()
+/**
+ * Hash2T is an implementation of hash table using two arrays synced by index with keys
+ * maintained in increasing order.
+ * @class
+ */
+this.Hash2T = function()
 {
 	let keys = [], values = []
 
 	/* Setter methods */
+	/**
+	 * set hash[key] to val
+	 *
+	 * @param key {any}  Key component of the associative table
+	 * @param val {any}  Value associated with key
+	 * @returns {undefined}
+	 */
 	this.set = function(key, val) {
 		let i = 0;
 		while (keys[i] !== undefined && keys[i] < key)
@@ -27,6 +36,12 @@ function Hash2T()
 		// console.log(keys, values);
 	}
 
+	/**
+	 * unset deletes the key/value association from the hashtable
+	 *
+	 * @param key {any}    Key componenent of association to be unset
+	 * @returns {undefined}
+	 */
 	this.unset = function(key) {
 		let i = 0;
 		while (keys[i] !== undefined && keys[i] != key)
@@ -39,12 +54,25 @@ function Hash2T()
 	}
 
 	/* Accessor methods */
+	/**
+	 * get  retrieves value association with key, if any
+	 *
+	 * @param key {any}   Key componenent of the association
+	 * @returns {any}  value if it exists, or undefined
+	 */
 	this.get = function(key) {
 		return keys.indexOf(key) !== -1 ?
 			values[keys.indexOf(key)] :
 			undefined;
 	}
 
+	/**
+	 * find   reverse-lookup to find key association with value, if any
+	 *
+	 * @param val {any}    Value to be searched for
+	 * @returns {any}   First key in hash that matches value, if any
+	 * TODO: take advantage of sort order
+	 */
 	this.find = function(val) {
 		return values.indexOf(val) !== -1 ?
 			keys[values.indexOf(val)] :
@@ -52,4 +80,4 @@ function Hash2T()
 	}
 }
 
-module.exports = Hash2T;
+module.exports = this.Hash2T;
